14b3b48b270b8faabf57de6dfbc2617ccabb40b8
leabevi <leabevi@gmail.com>
2021-10-29 12:11:29 -0300

    port fixed

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,8 +8,8 @@
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port =55502 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    const port = 8000 // Change this to your server port
+    return `https://leabevi.github.io/data/restaurants.json`;
   }
 
   /**
